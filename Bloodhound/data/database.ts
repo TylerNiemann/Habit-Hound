ca9907094ddceea67c8c785c6c0d801264778721
Tyler Niemann <tntman98@gmail.com>
2023-06-30 16:45:35 -0700

    add Daily Table with foreign key reference to Habit Table

--- Bloodhound/data/database.ts
+++ Bloodhound/data/database.ts
@@ -2,6 +2,10 @@ import * as SQLite from 'expo-sqlite';
 
 export const db = SQLite.openDatabase('habits.db');
 
+db.exec([{ sql: 'PRAGMA foreign_keys = ON;', args: [] }], false, () =>
+  console.log('Foreign keys turned on')
+);
+
 export function createHabitTable(habitName: string, timesPerWeek: number): void {
   const currentDate = new Date().toISOString();
 
@@ -22,6 +26,29 @@ export function createHabitTable(habitName: string, timesPerWeek: number): void 
       INSERT INTO Habit (name, created_date, times_per_week) VALUES (?, ?, ?);
       `,
       [habitName, currentDate, timesPerWeek],
+    );
+  });
+}
+ 
+
+export function createDailyTable(habit_id: number): void {
+  const currentDate = new Date().toISOString();
+
+  db.transaction((tx) => {
+    tx.executeSql(
+      `
+      CREATE TABLE IF NOT EXISTS Daily (
+        completed_date DATETIME,
+        habit_reference INTEGER REFERENCES Habit(habit_id)
+      );
+      `,
+      [],
+    );
+    tx.executeSql(
+      `
+      INSERT INTO Daily (completed_date, habit_reference) VALUES (?, ?);
+      `,
+      [currentDate, habit_id],
     );
   });
 }
